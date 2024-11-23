d8db094a507c63a75ca0b0a0fea199361ddda187
Hamza Sheikh <zafarhamza789@gmail.com>
2024-11-23 21:39:19 +0500

    navbar-completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Result from './Pages/Result'
 import BuyCredit from './Pages/BuyCredit'
+import NavBar from './Components/NavBar'
 
 const App = () => {
   return (
     <>
-      <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b
+       from-teal-50 to-orange-50'>
+
+        <NavBar />
+
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/result' element={<Result />} />
