5c398de955b1c5cd55719a8eec8a293cc2933622
Hamza Sheikh <zafarhamza789@gmail.com>
2024-11-24 21:19:13 +0500

    homePageCompleted

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './Pages/Home'
 import Result from './Pages/Result'
 import BuyCredit from './Pages/BuyCredit'
 import NavBar from './Components/NavBar'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
@@ -17,6 +18,8 @@ const App = () => {
           <Route path='/result' element={<Result />} />
           <Route path='/buy' element={<BuyCredit />} />
         </Routes>
+
+        <Footer/>
       </div>
     </>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,18 @@
+import Description from "../Components/Description"
+import GenerateBtn from "../Components/GenerateBtn"
 import Header from "../Components/Header"
+import Steps from "../Components/Steps"
+import Testimonials from "../Components/Testimonials"
 
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Steps/>
+      <Description/>
+      <Testimonials/>
+      <GenerateBtn/>
     </div>
   )
 }
